79d8072466dcb02a281a0daa36ff86f1a8c1de91
tsiapkinis <iason.tsiapkinis@ikz-berlin.de>
2025-12-09 12:22:00 +0100

    Create plot.py for temperature plots with pyvista helper functions in evaluation_pyvista.py

---
+++ evaluation_pyvista.py
@@ -0,0 +1,170 @@
+import pyvista as pv
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# def cylindrical_to_cartesian(r, phi, z):
+#     # COORDINATE TRANSFORMATION SIMULATION -> EXPERIMENT
+#     x = r * np.sin(phi)
+#     y = z
+#     z = r * np.cos(phi)
+#     return x, y, z
+
+
+def readAll_csv_data(filename):
+    data = pd.read_csv(filename)
+    return data
+
+def virtual_probe(pvd_file, point, probe_bounds = [-5e-3, 5e-3, -5e-3, 5e-3, -5e-3, 5e-3]):
+    # box_bounds: size of probe [xmin, xmax, ymin, ymax, zmin, zmax]
+    # x, y, z = cylindrical_to_cartesian(r, phi, z)
+    box_bounds = [point[0] + probe_bounds[0],
+                  point[0] + probe_bounds[1],
+                  point[1] + probe_bounds[2],
+                  point[1] + probe_bounds[3],
+                  point[2] + probe_bounds[4],
+                  point[2] + probe_bounds[5]]
+    print(box_bounds)
+    # Load the PVD file
+    
+    # pvd_data = pv.read(pvd_file)
+    reader = pv.read(pvd_file)
+    reader.set_active_time_point(-1)  # last time step (here: steady state iteration)
+    mesh = reader.read()
+    mesh = mesh.combine()  # combine different blocks (different vtu files) to one mesh
+    # # Access the "B" data from the PVD dataset
+    # b_data = pvd_data.point_arrays["magnetic flux density re e"]
+
+    # Create a box region of interest (ROI) using the given bounds
+    roi = pv.Box(bounds=box_bounds)
+
+    # Extract data within the ROI
+    extracted_data = roi.apply(dataset=reader)
+
+    # Calculate the mean value of the extracted "B" data
+    mean_value = np.mean(extracted_data.point_arrays["magnetic flux density im e"])
+
+    return mean_value
+
+def evaluate_circle(pvd_file, r, z, resolution=1000, plot=False):
+    # create circle
+    phi = np.linspace(-np.pi/2, 3/2*np.pi, resolution)
+    points = np.array([r*np.sin(phi), z*np.ones(phi.shape), r*np.cos(phi)]).T
+
+    # read pvd
+    reader = pv.get_reader(pvd_file)
+    reader.set_active_time_point(-1)  # last time step (here: steady state iteration)
+    mesh = reader.read()
+    mesh = mesh.combine()  # combine different blocks (different vtu files) to one mesh
+
+    # mesh.plot(style="wireframe", scalars="temperature", cmap="turbo")
+
+    # evaluate pvd
+    point_set = pv.PointSet(points)
+    probe = mesh.probe(point_set)
+    values = ((probe["magnetic flux density re e"]**2 + probe["magnetic flux density im e"]**2))**0.5
+    # values = probe["magnetic flux density im e"]
+    # print(points.shape[0])
+    # for i in range(0, points.shape[0]):
+    #     virtualProbe = virtual_probe(pvd_file, points[i,:])
+    #     print(f'point = {points[i,:]} \t values = {values[i,:]} \t virtualProbe = {virtualProbe[i,:]}')
+
+    if plot:
+        fig, ax = plt.subplots()
+        ax.plot(phi, values)
+        plt.show()
+    
+    return phi, values
+
+def evaluate_vertical_line(pvd_file, r, phi, z_start, z_end, resolution=100, plot=False):
+    # create line
+    z = np.linspace(z_start, z_end, resolution)
+    points = np.array([r*np.sin(phi)*np.ones(z.shape), z, r*np.cos(phi)*np.ones(z.shape)]).T
+
+    # read pvd
+    reader = pv.get_reader(pvd_file)
+    reader.set_active_time_point(-1)  # last time step (here: steady state iteration)
+    mesh = reader.read()
+    mesh = mesh.combine()  # combine different blocks (different vtu files) to one mesh
+
+    # evaluate pvd
+    point_set = pv.PointSet(points)
+    probe = mesh.probe(point_set)
+    values = probe["magnetic flux density im e"][:, 1]
+
+    if plot:
+        fig, ax = plt.subplots()
+        ax.plot(z, values)
+        plt.show()
+    
+    return z, values
+
+def evaluate_line(pvd_file, points, plot=False):
+    # read pvd
+    reader = pv.get_reader(pvd_file)
+    reader.set_active_time_point(-1)  # last time step (here: steady state iteration)
+    mesh = reader.read()
+    mesh = mesh.combine()  # combine different blocks (different vtu files) to one mesh
+
+    # evaluate pvd
+    point_set = pv.PointSet(points)
+    probe = mesh.probe(point_set)
+    values = ((probe["magnetic flux density re e"]**2 + probe["magnetic flux density im e"]**2))**0.5
+
+    if plot:
+        fig, ax = plt.subplots()
+        ax.plot(z, values)
+        plt.show()
+    
+    return points, values
+
+def evaluate_crystal_temperature(pvd_file, points, plot=False):
+    # read pvd
+    reader = pv.get_reader(pvd_file)
+    reader.set_active_time_point(-1)  # last time step (here: steady state iteration)
+    mesh = reader.read()
+    mesh = mesh.combine()  # combine different blocks (different vtu files) to one mesh
+
+    # evaluate pvd
+    point_set = pv.PointSet(points)
+    probe = mesh.probe(point_set)
+    values = probe["temperature"]
+
+    return points, values
+
+def evaluate_circle_temperature(pvd_file, r, z, resolution=1000, plot=False):
+    # create circle
+    phi = np.linspace(-np.pi/2, 3/2*np.pi, resolution)
+    points = np.array([r*np.sin(phi), z*np.ones(phi.shape), r*np.cos(phi)]).T
+
+    # read pvd
+    reader = pv.get_reader(pvd_file)
+    reader.set_active_time_point(-1)  # last time step (here: steady state iteration)
+    mesh = reader.read()
+    mesh = mesh.combine()  # combine different blocks (different vtu files) to one mesh
+
+    # mesh.plot(style="wireframe", scalars="temperature", cmap="turbo")
+
+    # evaluate pvd
+    point_set = pv.PointSet(points)
+    probe = mesh.probe(point_set)
+    values = probe["temperature"]
+    # values = ((probe["magnetic flux density re e"]**2 + probe["magnetic flux density im e"]**2))**0.5
+
+    if plot:
+        fig, ax = plt.subplots()
+        ax.plot(phi, values)
+        plt.show()
+    
+    return phi, values
+
+if __name__ == "__main__":
+    pvd_file = "simdata_size-factor=2_discontinuous-bodies/results/case.pvd"
+
+    y = 0.04  # height  (relative to vessel bottom)
+    r = 0.055  # radius
+    phi = 0
+    resolution = 100
+    # evlauate_circle(pvd_file, r, y, resolution, True)
+
+    evaluate_vertical_line(pvd_file, 0, 0, 0.05, 0.15, 100, True)
---
+++ plot.py
@@ -0,0 +1,138 @@
+import os
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import yaml
+import pandas as pd
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+# from matplotlib.ticker import FuncFormatter
+
+
+from evaluation_pyvista import evaluate_crystal_temperature, evaluate_circle_temperature
+from evaluation_meas3dfields import read_exp_data, find_unique_r_values, find_unique_z_values, readAll_exp_data
+
+colors = ["C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9"]
+font = {'size'   : 24}
+matplotlib.rc('font', **font)
+
+#### input
+simulation_dir = "."
+prefix = "res"
+
+#### calibration factors
+per = [2,0,1]
+####
+
+### sim data
+with open(f"{simulation_dir}/config_geo.yml") as f:
+        config_geo = yaml.safe_load(f)
+X0_feed = config_geo["fz_crystal"]["X0"]
+r_feed = config_geo["fz_crystal"]["r"]
+l_feed = config_geo["fz_crystal"]["l"]
+
+pvd_file = f"{simulation_dir}/simdata/results/case.pvd"
+
+z = np.linspace(X0_feed[1]*1, (X0_feed[1]+l_feed)*1, 1000)
+points = np.array([0*z, z, 0*z+r_feed*0.99]).T # at supply
+points2 = np.array([0*z, z, 0*z-r_feed*0.99]).T # at 180°
+points, T = evaluate_crystal_temperature(pvd_file, points)
+points2, T2 = evaluate_crystal_temperature(pvd_file, points2)
+T = T-273.15
+T2 = T2-273.15
+
+fig,ax = plt.subplots()
+ax.plot(T2, z*1000,color='tab:blue', label="3D-model")
+
+# ax.set_title("T on rod surface")
+ax.set_xlabel(f"$T$ [°C]")   
+ax.set_ylabel(f"$z$ [mm]")   
+ax.set_ylim([-100,100])
+ax.grid()
+legend = ax.legend(fontsize=12, loc="lower left")
+fig.tight_layout()
+fig.savefig(f"temperature.png", dpi=600)
+
+fig,ax = plt.subplots(figsize=(3.8,5))
+
+z = np.linspace(X0_feed[1]*1, (X0_feed[1]+l_feed)*1, 1000)
+points = np.array([0*z, z, 0*z+r_feed*0.99]).T # at supply
+points2 = np.array([0*z, z, 0*z-r_feed*0.99]).T # at 180°
+points, T = evaluate_crystal_temperature(pvd_file, points)
+points2, T2 = evaluate_crystal_temperature(pvd_file, points2)
+T = T-273.15
+T2 = T2-273.15
+
+print(T.max())
+print(T2.max())
+print(T.max()-T2.max())
+# window_size = 15
+# smoothed_signal = np.convolve(max_values_df.to_numpy().flatten(), np.ones(window_size)/window_size, mode='valid')
+# ax.plot(smoothed_signal, z_exp2[7:-7]*M-23,color='k', label="measured")
+ax.plot(T, z*1000,'--',color='tab:blue', label="3D-model a=0")
+ax.plot(T2, z*1000,color='tab:blue', label="3D-model a=180")
+
+# ax.set_title("T on rod surface")
+ax.set_xlabel(f"$T$ [°C]")   
+ax.set_ylabel(f"$z$ [mm]")   
+ax.set_ylim([-100,100])
+ax.grid()
+legend = ax.legend(fontsize=12, loc="lower left")
+fig.tight_layout()
+fig.savefig(f"temperature-2.png", dpi=600)
+
+
+phi, T_over_alpha = evaluate_circle_temperature(pvd_file, r_feed*0.99, 0, resolution=1000, plot=False)
+fig,ax = plt.subplots()
+ax.plot(phi*180/np.pi, T_over_alpha-273.15)
+
+ax.set_xlabel("$\\alpha$ [°]")
+ax.set_ylabel(f"$T$ [°C]")
+ax.grid()
+plt.tight_layout()
+fig.savefig("temperature-azimuthal.png")
+
+# Elmer:
+x_values = T2
+y_values = z * 1000
+idx45 = np.abs(y_values - 4.5).argmin()
+idx72 = np.abs(y_values - 72).argmin()
+idx0 = np.abs(y_values - 0).argmin()
+idx18 = np.abs(y_values - (-18)).argmin()
+idx30 = np.abs(y_values - (-30)).argmin()
+print(f"T(z={y_values[idx45]} = {x_values[idx45]}")
+print(f"T(z={y_values[idx72]} = {x_values[idx72]}")
+print(f"T(z={y_values[idx0]} = {x_values[idx0]}")
+print(f"T(z={y_values[idx18]} = {x_values[idx18]}")
+print(f"T(z={y_values[idx30]}) = {x_values[idx30]}")
+# Find the index of the maximum x value
+max_x_index = np.argmax(x_values)
+# Retrieve the corresponding x and y values
+max_x_value = x_values[max_x_index]
+max_y_value = y_values[max_x_index]
+# print(f"Maximum x value: {max_x_value}")
+# print(f"Corresponding y value: {max_y_value}")
+print(f"Tmax(z={max_y_value}) = {max_x_value}")
+
+## radiation-to-ambient:
+Tamb = 293.15
+epsilon = 0.064
+htc = 9.58
+kappa = 60
+sigmaB = 5.67074419e-8
+
+elmer_T = T2 + 273.15
+
+elmer_heatflux = (sigmaB * epsilon * (elmer_T**4 - Tamb**4) + htc*(elmer_T-Tamb))
+elmer_radiative = (sigmaB * epsilon * (elmer_T**4 - Tamb**4))
+
+print(np.trapz(elmer_heatflux,z))
+
+print(np.trapz(elmer_heatflux,z)*2*np.pi*r_feed)
+
+print(np.trapz(elmer_radiative,z)*2*np.pi*r_feed)
+
+plt.show()
+plt.close(fig)
+
+
+
